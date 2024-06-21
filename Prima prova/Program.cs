2b38349b9fa5b0ffd3e11bbbc983ded84a6f6c5b
Manuel Pinheiro <manuel.pinheiro@libero.it>
2024-06-21 11:34:37 +0200

    Aggiungere i file di progetto.

---
+++ Prima prova/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Prima_prova
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;// font per euro
+
+
+            Contribuente c = new Contribuente();
+            Console.WriteLine("inserisci i dati del contribuente");
+            Console.Write("Nome: ");
+            c.Nome = Console.ReadLine();
+
+            Console.Write("Cognome: ");
+            c.Cognome = Console.ReadLine();
+
+           Console.Write("Data di nascita (gg/mm/aaaa): ");
+           string dataNascitaInput = Console.ReadLine();
+           c.DataNascita = DateTime.ParseExact(dataNascitaInput, "dd/MM/yyyy", null);
+
+
+
+
+
+
+            Console.Write("sesso (uomo/donna/robot): ");
+            string sessoInput = Console.ReadLine();
+            c.Genere = (Sesso)Enum.Parse(typeof(Sesso), sessoInput, true);
+
+
+            Console.WriteLine("Codice fiscale: ");
+            c.Codice = Console.ReadLine(); 
+
+
+            Console.Write("Comune di residenza: ");
+            c.Comune = Console.ReadLine();
+
+
+            Console.Write("Reddito annuale: ");
+            c.Reddito = int.Parse(Console.ReadLine());
+
+            double imposta = c.calcoloImposta();
+
+
+            Console.WriteLine("================================ Cose di numeri ============================================");
+            Console.WriteLine("Calcolo imposta da versare:");
+            Console.WriteLine($"contribuente: {c.Nome} {c.Cognome}, ");
+            Console.WriteLine($"nato il {c.DataNascita.ToString("dd/MM/yyyy")} ({c.Genere})");
+            Console.WriteLine($"residente in {c.Comune}, ");
+            Console.WriteLine($"codice fiscale: {c.Codice}, ");
+            Console.WriteLine($"reddito dichiarato: £ {c.Reddito:0.00}");
+            Console.WriteLine($"IMPOSTA DA VERSARE: £ {imposta:0.00}");
+
+
+
+
+
+        }
+    }
+}
---
+++ Prima prova/classe.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prima_prova
+{
+
+    enum Sesso
+    {
+        Uomo,Donna,Robot
+    }
+
+    internal class Contribuente
+   
+    {
+
+
+        public string Nome { get; set; }
+      public string Cognome { get; set; }
+        public DateTime DataNascita { get; set; }
+        public Sesso Genere { get; set; }
+
+        public string Codice { get; set; }
+        public string Comune { get; set; }
+        public int Reddito { get; set; }
+
+       
+        public double calcoloImposta ()
+        {
+            double imposta;
+            if (Reddito <= 15000)
+            {
+                imposta = Reddito * 0.23;
+            }
+            else if (Reddito <= 28000)
+            {
+                imposta = 15000 * 0.23 + (Reddito - 15000) * 0.27;
+            }
+            else if (Reddito <= 55000)
+            {
+                imposta = 15000 * 0.23 + (28000 - 15000) * 0.27 + (Reddito - 28000) * 0.38;
+            }
+            else if (Reddito <= 75000)
+            {
+                imposta = 15000 * 0.23 + (28000 - 15000) * 0.27 + (55000 - 28000) * 0.38 + (Reddito - 55000) * 0.41;
+            }
+            else
+            {
+                imposta = 15000 * 0.23 + (28000 - 15000) * 0.27 + (55000 - 28000) * 0.38 + (75000 - 55000) * 0.41 + (Reddito - 75000) * 0.43;
+            }
+
+            return imposta;
+
+        }
+
+
+    }
+
+}
